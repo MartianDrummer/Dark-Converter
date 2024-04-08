01d8acdcabffcb9ca258c459082c6739b1bd34f4
MartianDrummer <marvinhill@mac.com>
2024-04-08 09:29:15 -0400

    Initial Commit

---
+++ Dark Converter/Dark_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dark_ConverterApp.swift
+//  Dark Converter
+//
+//  Created by Marvin on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Dark_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
